8139c1b41ccea9f37ccc393eae2a276b37c6ca16
LotaIbe <ibelota10@gmail.com>
2019-11-10 22:44:33 +0100

    Outline shape part 1

---
+++ 2 - outlineShape/part1/sketch.js
@@ -0,0 +1,79 @@
+
+const CRYSTAL_SIZE = 500 
+const SIDES = 6
+let PALETTE = []
+
+function setup() {
+  createCanvas(530, 530, SVG)
+
+  PALETTE = [
+    color(255, 52, 154), //pink
+    color(4, 0, 152), //blue
+    'limegreen'
+  ]
+  noLoop()
+  angleMode(DEGREES)
+  rectMode(CENTER)
+}
+
+function draw() {
+  // testLines()
+  outlineShape()
+} 
+
+
+
+function outlineShape () {
+const strokeColor = getRandomFromPalette()
+const weight = randomSelectTwo() ? 1 : 3
+const hexagonTrue =  randomSelectTwo()
+
+stroke(strokeColor)
+strokeWeight(weight)
+  push()
+  translate(width/2, height/2)
+  if (hexagonTrue) {
+    hexagon(0, 0, CRYSTAL_SIZE / 3)
+  } else {
+    ellipse(0,0, CRYSTAL_SIZE, CRYSTAL_SIZE)
+  }
+  pop()
+}
+
+// function to set design constraints
+function testLines () {
+let numShape = randomSelectTwo() ? SIDES : SIDES*2 // ternary operator, true = LHS, false = RHS
+// set stroke color to output from random palette color select function
+const strokeColor = getRandomFromPalette()
+console.log(strokeColor)
+
+  noFill() // no infil color
+  push() // isolate actions below
+    translate(width/2, height/2) // shift origin to center
+    stroke(PALETTE[0]) //pink color for circumference
+    ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE)
+    stroke(strokeColor) // random color for stroke selection
+    const angle = 360/numShape
+    for (let i = 0; i < numShape; i++){
+      line(0, 0,  CRYSTAL_SIZE/2, 0) // draw stroke from x0
+      rotate(angle) // rotate 
+    }
+  pop()
+}
+
+// function to generate random number between 0 and 1, returns true if result > 0.5 and vice versa
+function randomSelectTwo() {
+  const rando = random(1)
+  if (rando > 0.5) {
+    return true
+  } else {
+    return false
+  }
+} 
+
+// function to select random color from palette
+function getRandomFromPalette() {
+  const rando2 = floor(random(0, PALETTE.length))
+  return PALETTE[rando2]
+}
+
